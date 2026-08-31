81b98f23d1d5dda173b0235348b26df86b049779
dfanggara <dfanggara12@gmail.com>
2026-08-31 15:57:47 +0700

    fix: update initial HTML title in app.blade.php to Pintu Dua Coffeehouse

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>Pintu Dua Coffeehouse</title>
         <link rel="icon" type="image/png" href="/images/logo.png">
 
         <!-- Fonts & Icons -->
